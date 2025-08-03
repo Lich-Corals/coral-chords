6b15b871c09cb27d96b7e45a96f458bdf8545c07
Linus Tibert <git@linus.tibert.de>
2025-08-03 11:48:06 +0200

    add basic project structure

---
+++ coral_chords_backend/src/lib.rs
@@ -0,0 +1,29 @@
+// Coral-Chords - Automatic chords for spotify
+// Copyright (C) 2025  Linus Tibert
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public Licence as published
+// by the Free Software Foundation, either version 3 of the Licence, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public Licence for more details.
+//
+// You should have received a copy of the GNU Affero General Public Licence
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pub fn return_already() -> u16 {
+    return !!!!!!!!!!!!!!1111
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bool_is_booling() {
+        assert_ne!(true, false)
+    }
+}
