6c15baf779dfc3ea0526c262dd4190a13156cb48
Linus Tibert <git@linus.tibert.de>
2025-09-22 19:25:14 +0200

    move some settings to advanced section

--- src/ui_settings.rs
+++ src/ui_settings.rs
@@ -23,28 +23,113 @@ use iced::Alignment::Center;
 use iced::{color, font, Font};
 
 pub fn build_settings_page<'a>(application_state: &'a ApplicationState) -> Column<'a, Message> {
-        column![scrollable(
+        let mut col = column![column![
                 column![
-                        column![
-                                text("Application").size(25),
-                                row![
-                                        text("Theme"),
-                                        Space::new(10, 0),
-                                        combo_box(
-                                                &application_state.themes,
-                                                "Theme",
-                                                application_state.selected_theme.as_ref(),
-                                                Message::ApplyTheme
-                                        )
-                                        .width(300),
-                                ]
-                                .align_y(Center),
-                                row![checkbox(
-                                        "Notify about updates",
-                                        application_state.notify_about_updates
+                        text("Application").size(25),
+                        row![
+                                text("Theme"),
+                                Space::new(10, 0),
+                                combo_box(
+                                        &application_state.themes,
+                                        "Theme",
+                                        application_state.selected_theme.as_ref(),
+                                        Message::ApplyTheme
                                 )
-                                .on_toggle(Message::ToggleUpdateNotification),]
-                                .align_y(Center),
+                                .width(300),
+                        ]
+                        .align_y(Center),
+                        row![checkbox(
+                                "Notify about updates",
+                                application_state.notify_about_updates
+                        )
+                        .on_toggle(Message::ToggleUpdateNotification),]
+                        .align_y(Center),
+                ]
+                .spacing(20)
+                .align_x(Center),
+                column![
+                        text("Search").size(25),
+                        row![checkbox(
+                                "Only show downloadable search results",
+                                application_state.only_downloadable_results
+                        )
+                        .on_toggle(Message::SetDownloadableOnly),]
+                        .align_y(Center),
+                ]
+                .spacing(20)
+                .align_x(Center),
+                column![
+                        text("Tabs").size(25),
+                        row![
+                                text(format!("Text size: {:02}", application_state.tab_text_size)),
+                                Space::new(10, 0),
+                                slider(
+                                        5..=99,
+                                        application_state.tab_text_size,
+                                        Message::SetTabLineHeight
+                                )
+                                .width(300),
+                        ]
+                        .align_y(Center),
+                        row![
+                                text("Metadata colour:"),
+                                Space::new(10, 0),
+                                text_input("#209fb5", &application_state.metadata_colour_text)
+                                        .on_input(Message::RecolourMetadata)
+                                        .width(300),
+                        ]
+                        .align_y(Center),
+                        row![
+                                text("Header colour:"),
+                                Space::new(10, 0),
+                                text_input("#dc8a78", &application_state.header_colour_text)
+                                        .on_input(Message::HeaderColourChange)
+                                        .width(300),
+                        ]
+                        .align_y(Center),
+                        row![
+                                text("Chord colour:"),
+                                Space::new(10, 0),
+                                text_input("#fe640b", &application_state.chord_colour_text)
+                                        .on_input(Message::ChordColourChange)
+                                        .width(300),
+                        ]
+                        .align_y(Center),
+                        column![
+                                Space::new(0, 20),
+                                text("Some matadata\n ".to_string())
+                                        .size(application_state.tab_text_size as f32)
+                                        .font(Font::MONOSPACE)
+                                        .color(application_state.metadata_colour),
+                                rich_text([span("[Section header]")
+                                        .font(Font {
+                                                style: font::Style::Italic,
+                                                ..Font::MONOSPACE
+                                        })
+                                        .size(application_state.tab_text_size as f32)
+                                        .color(application_state.header_colour),]),
+                                text("A         C     A       B".to_string())
+                                        .size(application_state.tab_text_size as f32)
+                                        .font(Font::MONOSPACE)
+                                        .color(application_state.chord_colour),
+                                text("Tabs will look like this.".to_string())
+                                        .size(application_state.tab_text_size as f32)
+                                        .font(Font::MONOSPACE),
+                        ],
+                ]
+                .spacing(20)
+                .align_x(Center),
+        ]
+        .padding(10)
+        .spacing(20)
+        .align_x(Center)];
+
+        if application_state.show_advanced_settings {
+                col = column![
+                        col,
+                        Space::new(0, 20),
+                        column![
+                                text("Advanced").size(25),
                                 row![checkbox(
                                         "Log played songs locally",
                                         application_state.log_played_songs
@@ -77,17 +162,6 @@ pub fn build_settings_page<'a>(application_state: &'a ApplicationState) -> Colum
                                         .width(300),
                                 ]
                                 .align_y(Center),
-                        ]
-                        .spacing(20)
-                        .align_x(Center),
-                        column![
-                                text("Search").size(25),
-                                row![checkbox(
-                                        "Only show downloadable search results",
-                                        application_state.only_downloadable_results
-                                )
-                                .on_toggle(Message::SetDownloadableOnly),]
-                                .align_y(Center),
                                 row![checkbox(
                                         "Auto-clean search queries",
                                         application_state.clean_queries
@@ -108,11 +182,6 @@ pub fn build_settings_page<'a>(application_state: &'a ApplicationState) -> Colum
                                         .width(300),
                                 ]
                                 .align_y(Center),
-                        ]
-                        .spacing(20)
-                        .align_x(Center),
-                        column![
-                                text("Tabs").size(25),
                                 row![checkbox(
                                         "Remove lines before first chords",
                                         application_state.remove_first_lines
@@ -120,79 +189,22 @@ pub fn build_settings_page<'a>(application_state: &'a ApplicationState) -> Colum
                                 .on_toggle(Message::RemoveFirstLines),]
                                 .align_y(Center),
                                 row![checkbox(
-                                        "Remove empty lines",
+                                        "Remove empty lines from tabs",
                                         application_state.remove_empty_lines
                                 )
                                 .on_toggle(Message::RemoveEmptyLines),]
                                 .align_y(Center),
-                                row![
-                                        text(format!(
-                                                "Text size: {:02}",
-                                                application_state.tab_text_size
-                                        )),
-                                        Space::new(10, 0),
-                                        slider(
-                                                5..=99,
-                                                application_state.tab_text_size,
-                                                Message::SetTabLineHeight
-                                        )
-                                        .width(300),
-                                ]
-                                .align_y(Center),
-                                row![
-                                        text("Metadata colour:"),
-                                        Space::new(10, 0),
-                                        text_input(
-                                                "#209fb5",
-                                                &application_state.metadata_colour_text
-                                        )
-                                        .on_input(Message::RecolourMetadata)
-                                        .width(300),
-                                ]
-                                .align_y(Center),
-                                row![
-                                        text("Header colour:"),
-                                        Space::new(10, 0),
-                                        text_input(
-                                                "#dc8a78",
-                                                &application_state.header_colour_text
-                                        )
-                                        .on_input(Message::HeaderColourChange)
-                                        .width(300),
-                                ]
-                                .align_y(Center),
-                                row![
-                                        text("Chord colour:"),
-                                        Space::new(10, 0),
-                                        text_input("#fe640b", &application_state.chord_colour_text)
-                                                .on_input(Message::ChordColourChange)
-                                                .width(300),
-                                ]
-                                .align_y(Center),
-                                column![
-                                        Space::new(0, 20),
-                                        text("Some matadata\n ".to_string())
-                                                .size(application_state.tab_text_size as f32)
-                                                .font(Font::MONOSPACE)
-                                                .color(application_state.metadata_colour),
-                                        rich_text([span("[Section header]")
-                                                .font(Font {
-                                                        style: font::Style::Italic,
-                                                        ..Font::MONOSPACE
-                                                })
-                                                .size(application_state.tab_text_size as f32)
-                                                .color(application_state.header_colour),]),
-                                        text("A         C     A       B".to_string())
-                                                .size(application_state.tab_text_size as f32)
-                                                .font(Font::MONOSPACE)
-                                                .color(application_state.chord_colour),
-                                        text("Tabs will look like this.".to_string())
-                                                .size(application_state.tab_text_size as f32)
-                                                .font(Font::MONOSPACE),
-                                ],
                         ]
-                        .spacing(20)
-                        .align_x(Center),
+                        .align_x(Center)
+                        .spacing(20),
+                ]
+                .align_x(Center);
+        }
+
+        col = column![scrollable(
+                column![
+                        col,
+                        Space::new(0, 20),
                         column![
                                 Space::new(0, 20),
                                 row![
@@ -230,9 +242,9 @@ pub fn build_settings_page<'a>(application_state: &'a ApplicationState) -> Colum
                         ]
                         .align_x(Center),
                 ]
-                .padding(10)
-                .spacing(20)
-                .align_x(Center)
                 .width(application_state.size.width - 2.0 * application_state.main_padding)
-        )]
+                .align_x(Center)
+        )];
+
+        col
 }
